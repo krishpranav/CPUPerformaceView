8a9488845a7102ba684d9398f63d98edc29de3c7
krishpranav <krisna.pranav@gmail.com>
2021-05-21 14:04:18 +0530

    [+] Adding files [+]

---
+++ CPUPerformaceView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CPUPerformaceView
+//
+//  Created by Elangovan Ayyasamy on 21/05/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
